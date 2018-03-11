715bb5e09a766069f1248c1f6d77851ea049157c
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2018-03-11 08:50:32 +0100

    Skipping chunk rendering if chunk was rendered less than 30 seconds ago + ability to determine server name (it was tough)

--- src/main/java/pl/sebcel/minecraft/clientmaprenderer/ClientMapRendererMod.java
+++ src/main/java/pl/sebcel/minecraft/clientmaprenderer/ClientMapRendererMod.java
@@ -2,18 +2,23 @@ package pl.sebcel.minecraft.clientmaprenderer;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.imageio.ImageIO;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityEvent.EnteringChunk;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -22,7 +27,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ClientMapRendererMod.MODID, name = ClientMapRendererMod.NAME, version = ClientMapRendererMod.VERSION)
 public class ClientMapRendererMod {
-    
+
     public static final String MODID = "clientsidemaprenderer";
     public static final String NAME = "Client-Side Map Renderer";
     public static final String VERSION = "1.0";
@@ -39,25 +44,54 @@ public class ClientMapRendererMod {
     @SubscribeEvent
     public void handlePlayerEnteringChunkEvent(EnteringChunk event) {
         if (event.getEntity() instanceof EntityPlayer) {
+
+            System.out.println(getServerName());
+
             System.out.println("Player is entering chunk (" + event.getNewChunkX() + "," + event.getNewChunkZ() + ") from chunk (" + event.getOldChunkX() + "," + event.getOldChunkZ() + ") " + event.getPhase());
-            World world = event.getEntity().getEntityWorld();
-            scanTerrain(world, event.getNewChunkX()-1, event.getNewChunkZ()-1);
-            scanTerrain(world, event.getNewChunkX()-1, event.getNewChunkZ());
-            scanTerrain(world, event.getNewChunkX()-1, event.getNewChunkZ()+1);
-            scanTerrain(world, event.getNewChunkX(), event.getNewChunkZ()-1);
-            scanTerrain(world, event.getNewChunkX(), event.getNewChunkZ());
-            scanTerrain(world, event.getNewChunkX(), event.getNewChunkZ()+1);
-            scanTerrain(world, event.getNewChunkX()+1, event.getNewChunkZ()-1);
-            scanTerrain(world, event.getNewChunkX()+1, event.getNewChunkZ());
-            scanTerrain(world, event.getNewChunkX()+1, event.getNewChunkZ()+1);
+
+            try {
+                World world = event.getEntity().getEntityWorld();
+                scanTerrain(world, new ChunkPos(event.getNewChunkX() - 1, event.getNewChunkZ() - 1));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX() - 1, event.getNewChunkZ()));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX() - 1, event.getNewChunkZ() + 1));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX(), event.getNewChunkZ() - 1));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX(), event.getNewChunkZ()));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX(), event.getNewChunkZ() + 1));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX() + 1, event.getNewChunkZ() - 1));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX() + 1, event.getNewChunkZ()));
+                scanTerrain(world, new ChunkPos(event.getNewChunkX() + 1, event.getNewChunkZ() + 1));
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
         }
     }
-    
-    private Set<IBlockState> blockStates = new HashSet<>();
 
-    public void scanTerrain(World world, int chunkX, int chunkZ) {
-        System.out.println("Scanning world in chunk (" + chunkX + "," + chunkZ + ")");
-        Chunk chunk = world.getChunkFromChunkCoords(chunkX, chunkZ);
+    private String getServerName() {
+        try {
+            if (FMLCommonHandler.instance().getMinecraftServerInstance() != null) {
+                return "local_" + FMLCommonHandler.instance().getMinecraftServerInstance().getFolderName();
+            } else if (Minecraft.getMinecraft().getCurrentServerData() != null) {
+                return "server_ " + Minecraft.getMinecraft().getCurrentServerData().serverName;
+            } else {
+                return "unknown";
+            }
+        } catch (Throwable ex) {
+            return "unknown";
+        }
+    }
+
+    private Set<IBlockState> blockStates = new HashSet<>();
+    private Map<ChunkPos, Date> lastRenderTime = new HashMap<>();
+    private final static int chunkImageTTLinSeconds = 30;
+
+    public void scanTerrain(World world, ChunkPos chunkPosition) {
+        if (chunkRenderedRecently(chunkPosition)) {
+            System.out.println("Skipping chunk (" + chunkPosition.x + "," + chunkPosition.z + ")");
+            return;
+        }
+
+        System.out.println("Scanning world in chunk (" + chunkPosition.x + "," + chunkPosition.z + ")");
+        Chunk chunk = world.getChunkFromChunkCoords(chunkPosition.x, chunkPosition.z);
         int[] heightMap = chunk.getHeightMap();
         int[] colorMap = new int[256];
         for (int x = 0; x < 16; x++) {
@@ -78,15 +112,24 @@ public class ClientMapRendererMod {
             }
         }
 
-        renderChunk(heightMap, colorMap, chunkX, chunkZ);
-        
-        for (IBlockState blockState : blockStates) {
-            System.out.println(blockState.getBlock().getLocalizedName()+" " + Block.getIdFromBlock(blockState.getBlock()));
-        }
-        
+        renderChunk(heightMap, colorMap, chunkPosition);
+        lastRenderTime.put(chunkPosition, new Date());
+
     }
 
-    private void renderChunk(int[] heightMap, int[] colorMap, int chunkX, int chunkZ) {
+    private boolean chunkRenderedRecently(ChunkPos chunkPosition) {
+        Date lastRenderingTime = lastRenderTime.get(chunkPosition);
+        Date now = new Date();
+        if (lastRenderingTime != null) {
+            long dt = now.getTime() - lastRenderingTime.getTime();
+            System.out.println("Time: " + dt);
+            return dt < chunkImageTTLinSeconds * 1000;
+        } else {
+            return false;
+        }
+    }
+
+    private void renderChunk(int[] heightMap, int[] colorMap, ChunkPos chunkPosition) {
         try {
             BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB);
 
@@ -97,7 +140,7 @@ public class ClientMapRendererMod {
                 }
             }
 
-            String fileName = "chunk_" + chunkX + "_" + chunkZ + ".png";
+            String fileName = "chunk_" + chunkPosition.x + "_" + chunkPosition.z + ".png";
             File outputFile = new File(fileName);
             System.out.println("Writing chunk to " + outputFile.getAbsolutePath());
             ImageIO.write(image, "png", outputFile);
